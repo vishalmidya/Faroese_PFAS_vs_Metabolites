fc4315a1808718e69ab49032b2da320096151948
Midya <vishal.midya@mssm.edu>
2022-10-14 14:45:56 -0400

    pfoa 28 matched

--- HILIC/pfos/pfos_28/pfos_28_matching.R
+++ HILIC/pfos/pfos_28/pfos_28_matching.R
@@ -3,12 +3,14 @@
 library(optmatch)
 library(MatchIt)
 
+
 sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age7
 
+merged_omics <- read.csv("C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/New_faroese/HILIC/merged_omics_hilic.csv", check.names = F)
 
-m.out1.pfos28_age28 <- matchit(cpfos28 ~  sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age28, 
+m.out1.pfos28_age28 <- matchit(cpfos28 ~  sex + mage + cmatfishpreg  + cparity + age28, 
                               data = merged_omics[merged_omics$Year == 28,], discard = "both", method = "full", 
-                              distance = "glm", caliper = 0.5)
+                              distance = "glm", caliper = 0.1)
 
 summary(m.out1.pfos28_age28)
 f <- love.plot(m.out1.pfos28_age28)
@@ -17,5 +19,5 @@ f + labs(title  = " ", x= "Standardized Mean Difference") + geom_vline(xintercep
 
 
 m.out1.pfos28_age28.matched <- match.data(m.out1.pfos28_age28)
-write.csv(m.out1.pfos28_age28.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/Faroese data/all pfas/pfos/pfos at age 28/minerva_data_pfos_28_metabolite_28/matched_data_pfos_at_28_met_at_28.csv",
+write.csv(m.out1.pfos28_age28.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/New_faroese/HILIC/pfos/pfos_28/minerva_data_pfos_28_metabolite_28/matched_data_pfos_at_28_met_at_28.csv",
           row.names = F)
