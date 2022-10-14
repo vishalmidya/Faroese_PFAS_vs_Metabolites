6fabfcb36502c0f459c06aebab98f2e881b44201
Vishal Midya <39417940+vishalmidya@users.noreply.github.com>
2022-10-14 14:08:17 -0400

    Update pfos_14_matching.R

--- HILIC/pfos/pfos_14/pfos_14_matching.R
+++ HILIC/pfos/pfos_14/pfos_14_matching.R
@@ -6,58 +6,44 @@ library(MatchIt)
 
 sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age7
 
-merged_omics <- read.csv("C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/New_faroese/HILIC/merged_omics_hilic.csv", check.names = F)
-
-
-m.out1.pfos7_age7 <- matchit(cpfos7 ~ sex + mage   + smokepreg_2 + cmatfishpreg  + cparity  , 
-                             data = merged_omics[merged_omics$Year == 7,], discard = "both", method = "full",
-                             distance = "glm", caliper = 0.5)
-
-summary(m.out1.pfos7_age7)
-f <- love.plot(m.out1.pfos7_age7)
-f + labs(title  = "caliper = 0.5\n Metabolites at age 7 vs. pfos ", x= "Standardized Mean Difference") + geom_vline(xintercept  = 0.1 , linetype="dotted",  color = "black", size=1.5) + geom_vline(xintercept  = -0.1 , linetype="dotted", color = "black", size=1.5)
-
-
-m.out1.pfos7_age14 <- matchit(cpfos7 ~ sex + mage + mbmi  + smokepreg_2   + cparity  ,
+m.out1.pfos14_age14 <- matchit(cpfos14 ~    sex + mage + mbmi   + cmatfishpreg  + cparity + age14,
                               data = merged_omics[merged_omics$Year == 14,], discard = "both", method = "full", 
-                              distance = "glm", caliper = 0.5)
+                              distance = "glm", caliper = 0.1)
 
-summary(m.out1.pfos7_age14)
-f <- love.plot(m.out1.pfos7_age14)
+summary(m.out1.pfos14_age14)
+f <- love.plot(m.out1.pfos14_age14)
 f + labs(title  = " ", x= "Standardized Mean Difference") + geom_vline(xintercept  = 0.1 , linetype="dotted",  color = "black", size=1.5) + geom_vline(xintercept  = -0.1 , linetype="dotted", color = "black", size=1.5)
 
 
-m.out1.pfos7_age22 <- matchit(cpfos7 ~ sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age22, 
+m.out1.pfos14_age22 <- matchit(cpfos14 ~  sex  + mbmi  + cmatfishpreg  + cparity + age22, 
                               data = merged_omics[merged_omics$Year == 22,], discard = "both", method = "full", 
                               distance = "glm", caliper = 0.5)
 
-summary(m.out1.pfos7_age22)
-f <- love.plot(m.out1.pfos7_age22)
+summary(m.out1.pfos14_age22)
+f <- love.plot(m.out1.pfos14_age22)
 f + labs(title  = " ", x= "Standardized Mean Difference") + geom_vline(xintercept  = 0.1 , linetype="dotted",  color = "black", size=1.5) + geom_vline(xintercept  = -0.1 , linetype="dotted", color = "black", size=1.5)
 
 
-m.out1.pfos7_age28 <- matchit(cpfos7 ~  sex + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age28, 
+m.out1.pfos14_age28 <- matchit(cpfos14 ~  sex + mage   + smokepreg_2 + cmatfishpreg  + cparity + age28, 
                               data = merged_omics[merged_omics$Year == 28,], discard = "both", method = "full", 
-                              distance = "glm", caliper = 0.05)
+                              distance = "glm", caliper = 0.5)
 
-summary(m.out1.pfos7_age28)
-f <- love.plot(m.out1.pfos7_age28)
+summary(m.out1.pfos14_age28)
+f <- love.plot(m.out1.pfos14_age28)
 f + labs(title  = " ", x= "Standardized Mean Difference") + geom_vline(xintercept  = 0.1 , linetype="dotted",  color = "black", size=1.5) + geom_vline(xintercept  = -0.1 , linetype="dotted", color = "black", size=1.5)
 
-m.out1.pfos7_age7.matched <- match.data(m.out1.pfos7_age7)
-write.csv(m.out1.pfos7_age7.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/New_faroese/HILIC/pfos/pfos_7/minerva_data_pfos_7_metabolite_7/matched_data_pfos_at_7_met_at_7.csv",
+
+m.out1.pfos14_age14.matched <- match.data(m.out1.pfos14_age14)
+write.csv(m.out1.pfos14_age14.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/Faroese data/all pfas/pfos/pfos at age 14/minerva_data_pfos_14_metabolite_14/matched_data_pfos_at_14_met_at_14.csv",
           row.names = F)
 
-m.out1.pfos7_age14.matched <- match.data(m.out1.pfos7_age14)
-write.csv(m.out1.pfos7_age14.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/New_faroese/HILIC/pfos/pfos_7/minerva_data_pfos_7_metabolite_14/matched_data_pfos_at_7_met_at_14.csv",
-          row.names = F)
-
-m.out1.pfos7_age22.matched <- match.data(m.out1.pfos7_age22)
-write.csv(m.out1.pfos7_age22.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/New_faroese/HILIC/pfos/pfos_7/minerva_data_pfos_7_metabolite_22/matched_data_pfos_at_7_met_at_22.csv",
+m.out1.pfos14_age22.matched <- match.data(m.out1.pfos14_age22)
+write.csv(m.out1.pfos14_age22.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/Faroese data/all pfas/pfos/pfos at age 14/minerva_data_pfos_14_metabolite_22/matched_data_pfos_at_14_met_at_22.csv",
           row.names = F)
 
 
-m.out1.pfos7_age28.matched <- match.data(m.out1.pfos7_age28)
-write.csv(m.out1.pfos7_age28.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/New_faroese/HILIC/pfos/pfos_7/minerva_data_pfos_7_metabolite_28/matched_data_pfos_at_7_met_at_28.csv",
+m.out1.pfos14_age28.matched <- match.data(m.out1.pfos14_age28)
+write.csv(m.out1.pfos14_age28.matched, "C:/Users/midyav01/OneDrive - The Mount Sinai Hospital/MSSM Projects/METABOLOMICS/Faroese data/all pfas/pfos/pfos at age 14/minerva_data_pfos_14_metabolite_28/matched_data_pfos_at_14_met_at_28.csv",
           row.names = F)
+
 
