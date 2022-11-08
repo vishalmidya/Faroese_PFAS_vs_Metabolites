759483ab4d7a2a683f625366737eb740c2ad2d1d
Midya <vishal.midya@mssm.edu>
2022-11-08 17:00:07 -0500

    creating fisher p-value files

--- HILIC/pfos/pfos_7/minerva_data_pfos_7_metabolite_22/pfos_7_met_22_fisher_pvalue_hilic.R
+++ HILIC/pfos/pfos_7/minerva_data_pfos_7_metabolite_22/pfos_7_met_22_fisher_pvalue_hilic.R
@@ -16,15 +16,15 @@ registerDoParallel(cl)
 start.time <- Sys.time()
 
 data_hilic <- read.csv("/sc/arion/work/yaom03/new_faroese/hilic/data_hilic.csv", check.names = F)
-m.out1.pfos7_age22.matched <- read.csv("/sc/arion/work/yaom03/new_faroese/hilic/pfos/pfos_7/minerva_data_pfos_7_metabolites_22/matched_data_pfos_at_0_met_at_22.csv")
+m.out1.pfos_7_age22.matched <- read.csv("/sc/arion/work/yaom03/new_faroese/hilic/pfos/pfos_7/minerva_data_pfos_7_metabolites_22/matched_data_pfos_at_7_met_at_22.csv")
 
-data = m.out1.pfos7_age22.matched[,c(paste0("Met",seq(1:nrow(data_hilic))), 'cpfos7', 'sex',
+data = m.out1.pfos_7_age22.matched[,c(paste0("Met",seq(1:nrow(data_hilic))), 'cpfos_7', 'sex',
                                     'mage',  'mbmi', 'smokepreg_2', 'cmatfishpreg', 'cparity', 'age22' )]
 
-data.pfos.7.met_at_22 <- cbind(data_hilic[,c("mz","time","KEGG","Annotation.confidence.score","chem_name","Met_id")])
-data.pfos.7.met_at_22$beta <- rep(NA_real_, nrow(data.pfos.7.met_at_22))
-data.pfos.7.met_at_22$model_pval <- rep(NA_real_, nrow(data.pfos.7.met_at_22))
-data.pfos.7.met_at_22$simu_pval <- rep(NA_real_, nrow(data.pfos.7.met_at_22))
+data.pfos_7.met_at_22 <- cbind(data_hilic[,c("mz","time","KEGG","Annotation.confidence.score","chem_name","Met_id")])
+data.pfos_7.met_at_22$beta <- rep(NA_real_, nrow(data.pfos_7.met_at_22))
+data.pfos_7.met_at_22$model_pval <- rep(NA_real_, nrow(data.pfos_7.met_at_22))
+data.pfos_7.met_at_22$simu_pval <- rep(NA_real_, nrow(data.pfos_7.met_at_22))
 
 comb <- function(x, ...) {
   lapply(seq_along(x),
@@ -33,16 +33,16 @@ comb <- function(x, ...) {
 
 # Model test statistic and raw p-value
 
-oper <- foreach(i=1:nrow(data.pfos.7.met_at_22), .combine='comb', .multicombine=TRUE,
+oper <- foreach(i=1:nrow(data.pfos_7.met_at_22), .combine='comb', .multicombine=TRUE,
                 .init=list(list(), list())) %dopar% {
                   exposure <- paste0("Met",i)
-                  model <- (lm(data[,i] ~ cpfos7 + sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age22, data = data))
+                  model <- (lm(data[,i] ~ cpfos_7 + sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age22, data = data))
                   s <- summary(model)
                   list(s$coefficients[2,"Estimate"], s$coefficients[2,"Pr(>|t|)"])
                 }
 
-data.pfos.7.met_at_22$beta <- unlist(oper[[1]])
-data.pfos.7.met_at_22$model_pval <- unlist(oper[[2]])
+data.pfos_7.met_at_22$beta <- unlist(oper[[1]])
+data.pfos_7.met_at_22$model_pval <- unlist(oper[[2]])
 observed_test_stat <- unlist(oper[[1]])
 
 
@@ -53,9 +53,9 @@ test_stat <- rep(NA_real_, iterations)
 
 set.seed(runif(1,0,1e4))
 data_permuted <- data
-system.time(test_stat <- foreach(i=1:nrow(data.pfos.7.met_at_22), .combine='rbind', .multicombine=TRUE) %:% 
+system.time(test_stat <- foreach(i=1:nrow(data.pfos_7.met_at_22), .combine='rbind', .multicombine=TRUE) %:% 
               foreach(j = 1:iterations, .combine = 'c') %dopar% {
-                f_perm <- lm(sample(data_permuted[,i]) ~ cpfos7 + sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age22, data=data_permuted)
+                f_perm <- lm(sample(data_permuted[,i]) ~ cpfos_7 + sex + mage + mbmi  + smokepreg_2 + cmatfishpreg  + cparity + age22, data=data_permuted)
                 s_f_perm <- summary(f_perm)
                 s_f_perm$coefficients[2,"Estimate"]
                 
@@ -71,12 +71,12 @@ rownames(test_stat_table) <- colnames(test_stat)
 write.table(test_stat_table,"/sc/arion/work/yaom03/new_faroese/hilic/pfos/pfos_7/minerva_data_pfos_7_metabolites_22/pfos_7_met_22_hypothetical_test_stat_hilic.txt", row.names = FALSE)
 
 
-p_values <- foreach(p = 1:dim(data.pfos.7.met_at_22)[1], .combine = 'c') %dopar% {
+p_values <- foreach(p = 1:dim(data.pfos_7.met_at_22)[1], .combine = 'c') %dopar% {
   mean(abs(test_stat_table[,p]) >= abs(observed_test_stat[p]))
 }
-data.pfos.7.met_at_22$simu_pval <- p_values
+data.pfos_7.met_at_22$simu_pval <- p_values
 
-write.table(data.pfos.7.met_at_22,"/sc/arion/work/yaom03/new_faroese/hilic/pfos/pfos_7/minerva_data_pfos_7_metabolites_22/pfos_7_met_22_beta_fisher_hilic.txt", row.names = FALSE)
+write.table(data.pfos_7.met_at_22,"/sc/arion/work/yaom03/new_faroese/hilic/pfos/pfos_7/minerva_data_pfos_7_metabolites_22/pfos_7_met_22_beta_fisher_hilic.txt", row.names = FALSE)
 
 end.time <- Sys.time()
 (time.taken <- end.time - start.time)
