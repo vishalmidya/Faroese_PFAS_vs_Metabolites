96b48df512a359812ced87c0c333635b1e37eb8c
Vishal Midya <39417940+vishalmidya@users.noreply.github.com>
2023-01-02 12:31:05 -0500

    changing the rand_adj_pval

--- combined_pvalues/randomized_adj_pvalues/pfos/pfos_22/pfos_22_combine.R
+++ combined_pvalues/randomized_adj_pvalues/pfos/pfos_22/pfos_22_combine.R
@@ -41,9 +41,9 @@ adj_pval <- foreach(i = 1:length(fisher_p_val), .combine = 'c') %dopar% {
   mean(min_p_nrep <= fisher_p_val[i])
 }
 
-p4$rand_adj_pval <- adj_pval[1:length(p4$simu_pval)]
+p4$rand_adj_pval <- adj_pval[1:1991]
 
-p8$rand_adj_pval <- adj_pval[(1*length(p4$simu_pval)+1): (1*length(p4$simu_pval) + length(p8$simu_pval))]
+p8$rand_adj_pval <- adj_pval[1992: 2778]
 
 
 write.table(p4,"/sc/arion/projects/Faroese/hilic/pfos/pfos_22/minerva_data_pfos_22_metabolites_28/pfos_22_met_28_beta_fisher_hilic.txt", row.names = FALSE)
