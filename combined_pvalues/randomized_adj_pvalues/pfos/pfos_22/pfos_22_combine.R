de697138136cda731d03229f33f3003273b79166
Yao <meizhen.yao@mssm.edu>
2023-01-04 13:39:34 -0500

    all combine code

--- combined_pvalues/randomized_adj_pvalues/pfos/pfos_22/pfos_22_combine.R
+++ combined_pvalues/randomized_adj_pvalues/pfos/pfos_22/pfos_22_combine.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(foreach)
 library(doParallel)
 library(car)
@@ -55,66 +54,3 @@ end.time <- Sys.time()
 
 stopCluster(cl)
 
-
-
-=======
-library(foreach)
-library(doParallel)
-library(car)
-library(MASS)
-library(ggplot2)
-library(data.table)
-
-cl <- makeCluster(10) 
-registerDoParallel(cl)
-
-start.time <- Sys.time()
-
-d4 <- fread("/sc/arion/projects/Faroese/hilic/pfos/pfos_22/minerva_data_pfos_22_metabolites_28/pfos_22_met_28_hypothetical_test_stat_hilic.txt")
-
-d8 <- fread("/sc/arion/projects/Faroese/c18/pfos/pfos_22/minerva_data_pfos_22_metabolites_28/pfos_22_met_28_hypothetical_test_stat_c18.txt")
-
-test_stat_table <- as.data.frame(cbind(d4, d8))
-
-
-p4 <- fread("/sc/arion/projects/Faroese/hilic/pfos/pfos_22/minerva_data_pfos_22_metabolites_28/pfos_22_met_28_beta_fisher_hilic.txt")
-
-p8 <- fread("/sc/arion/projects/Faroese/c18/pfos/pfos_22/minerva_data_pfos_22_metabolites_28/pfos_22_met_28_beta_fisher_c18.txt")
-
-fisher_p_val <- c(p4$simu_pval, p8$simu_pval)
-
-
-hyp_p_val <- foreach(r = 1:ncol(test_stat_table), .combine = 'cbind') %dopar% {
-  1 - rank(abs(test_stat_table[,r]))/nrow(test_stat_table) + 1/nrow(test_stat_table)
-}
-hyp_p_val <- as.data.frame(hyp_p_val)
-
-
-min_p_nrep <- foreach(p = 1:dim(hyp_p_val)[1], .combine = 'c') %dopar% {
-  min(hyp_p_val[p,], na.rm = T)
-}
-
-write.table(hyp_p_val,"/sc/arion/projects/Faroese/combined_pvalues/randomized_adj_pvalues/pfos/pfos_22/pfos_22_hypothetical_pval.txt", row.names = FALSE)
-
-# calculate the proportion of min_p_nrep that is sm/eq. p_value (for obs.)
-adj_pval <- foreach(i = 1:length(fisher_p_val), .combine = 'c') %dopar% {
-  mean(min_p_nrep <= fisher_p_val[i])
-}
-
-p4$rand_adj_pval <- adj_pval[1:1991]
-
-p8$rand_adj_pval <- adj_pval[1992: 2778]
-
-
-write.table(p4,"/sc/arion/projects/Faroese/hilic/pfos/pfos_22/minerva_data_pfos_22_metabolites_28/pfos_22_met_28_beta_fisher_hilic.txt", row.names = FALSE)
-
-write.table(p8,"/sc/arion/projects/Faroese/c18/pfos/pfos_22/minerva_data_pfos_22_metabolites_28/pfos_22_met_28_beta_fisher_c18.txt", row.names = FALSE)
-
-end.time <- Sys.time()
-(time.taken <- end.time - start.time)
-
-stopCluster(cl)
-
-
-
->>>>>>> 67ffe05a7f01b430a6da9587c69498d5180930d9
