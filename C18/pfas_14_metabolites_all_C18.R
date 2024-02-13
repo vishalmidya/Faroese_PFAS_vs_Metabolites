1b01ff0b6af9f0a6df23bcfe6b7204743aabbbcf
Meizhen Yao <109821933+MeizhenYao@users.noreply.github.com>
2024-02-12 20:35:22 -0500

    update combine file

--- C18/pfas_14_metabolites_all_C18.R
+++ C18/pfas_14_metabolites_all_C18.R
@@ -61,13 +61,13 @@ library(ggrepel)
 ### pfoa
 #### met at 22
 
-data.pfoa.14.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfoa\\pfoa_14\\minerva_data_pfoa_14_metabolites_22\\pfoa_14_met_22_beta_fisher_C18.txt", header=TRUE)
+data.pfoa.14.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfoa/pfoa_14/minerva_data_pfoa_14_metabolites_22/pfoa_14_met_22_beta_fisher_C18.txt", header=TRUE)
 data.pfoa.14.met_at_22$PFAS <-  rep("pfoa", nrow(data.pfoa.14.met_at_22))
 data.pfoa.14.met_at_22$Age = rep("22", nrow(data.pfoa.14.met_at_22))
 
 #### met at 28
 
-data.pfoa.14.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfoa\\pfoa_14\\minerva_data_pfoa_14_metabolites_28\\pfoa_14_met_28_beta_fisher_C18.txt", header=TRUE)
+data.pfoa.14.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfoa/pfoa_14/minerva_data_pfoa_14_metabolites_28/pfoa_14_met_28_beta_fisher_C18.txt", header=TRUE)
 data.pfoa.14.met_at_28$PFAS <-  rep("pfoa", nrow(data.pfoa.14.met_at_28))
 data.pfoa.14.met_at_28$Age = rep("28", nrow(data.pfoa.14.met_at_28))
 
@@ -76,13 +76,13 @@ pfoa_met <- rbind(data.pfoa.14.met_at_22, data.pfoa.14.met_at_28)
 ### pfos
 #### met at 22
 
-data.pfos.14.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfos\\pfos_14\\minerva_data_pfos_14_metabolites_22\\pfos_14_met_22_beta_fisher_C18.txt", header=TRUE)
+data.pfos.14.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfos/pfos_14/minerva_data_pfos_14_metabolites_22/pfos_14_met_22_beta_fisher_C18.txt", header=TRUE)
 data.pfos.14.met_at_22$PFAS <-  rep("pfos", nrow(data.pfos.14.met_at_22))
 data.pfos.14.met_at_22$Age = rep("22", nrow(data.pfos.14.met_at_22))
 
 #### met at 28
 
-data.pfos.14.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfos\\pfos_14\\minerva_data_pfos_14_metabolites_28\\pfos_14_met_28_beta_fisher_C18.txt", header=TRUE)
+data.pfos.14.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfos/pfos_14/minerva_data_pfos_14_metabolites_28/pfos_14_met_28_beta_fisher_C18.txt", header=TRUE)
 data.pfos.14.met_at_28$PFAS <-  rep("pfos", nrow(data.pfos.14.met_at_28))
 data.pfos.14.met_at_28$Age = rep("28", nrow(data.pfos.14.met_at_28))
 
@@ -91,13 +91,13 @@ pfos_met <- rbind(data.pfos.14.met_at_22, data.pfos.14.met_at_28)
 ### pfna
 #### met at 22
 
-data.pfna.14.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfna\\pfna_14\\minerva_data_pfna_14_metabolites_22\\pfna_14_met_22_beta_fisher_C18.txt", header=TRUE)
+data.pfna.14.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfna/pfna_14/minerva_data_pfna_14_metabolites_22/pfna_14_met_22_beta_fisher_C18.txt", header=TRUE)
 data.pfna.14.met_at_22$PFAS <-  rep("pfna", nrow(data.pfna.14.met_at_22))
 data.pfna.14.met_at_22$Age = rep("22", nrow(data.pfna.14.met_at_22))
 
 #### met at 28
 
-data.pfna.14.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfna\\pfna_14\\minerva_data_pfna_14_metabolites_28\\pfna_14_met_28_beta_fisher_C18.txt", header=TRUE)
+data.pfna.14.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfna/pfna_14/minerva_data_pfna_14_metabolites_28/pfna_14_met_28_beta_fisher_C18.txt", header=TRUE)
 data.pfna.14.met_at_28$PFAS <-  rep("pfna", nrow(data.pfna.14.met_at_28))
 data.pfna.14.met_at_28$Age = rep("28", nrow(data.pfna.14.met_at_28))
 
@@ -106,13 +106,13 @@ pfna_met <- rbind(data.pfna.14.met_at_22, data.pfna.14.met_at_28)
 ### pfda
 #### met at 22
 
-data.pfda.14.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfda\\pfda_14\\minerva_data_pfda_14_metabolites_22\\pfda_14_met_22_beta_fisher_C18.txt", header=TRUE)
+data.pfda.14.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfda/pfda_14/minerva_data_pfda_14_metabolites_22/pfda_14_met_22_beta_fisher_C18.txt", header=TRUE)
 data.pfda.14.met_at_22$PFAS <-  rep("pfda", nrow(data.pfda.14.met_at_22))
 data.pfda.14.met_at_22$Age = rep("22", nrow(data.pfda.14.met_at_22))
 
 #### met at 28
 
-data.pfda.14.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfda\\pfda_14\\minerva_data_pfda_14_metabolites_28\\pfda_14_met_28_beta_fisher_C18.txt", header=TRUE)
+data.pfda.14.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfda/pfda_14/minerva_data_pfda_14_metabolites_28/pfda_14_met_28_beta_fisher_C18.txt", header=TRUE)
 data.pfda.14.met_at_28$PFAS <-  rep("pfda", nrow(data.pfda.14.met_at_28))
 data.pfda.14.met_at_28$Age = rep("28", nrow(data.pfda.14.met_at_28))
 
@@ -121,13 +121,13 @@ pfda_met <- rbind(data.pfda.14.met_at_22, data.pfda.14.met_at_28)
 ### pfhxs
 #### met at 22
 
-data.pfhxs.14.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfhxs\\pfhxs_14\\minerva_data_pfhxs_14_metabolites_22\\pfhxs_14_met_22_beta_fisher_C18.txt", header=TRUE)
+data.pfhxs.14.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfhxs/pfhxs_14/minerva_data_pfhxs_14_metabolites_22/pfhxs_14_met_22_beta_fisher_C18.txt", header=TRUE)
 data.pfhxs.14.met_at_22$PFAS <-  rep("pfhxs", nrow(data.pfhxs.14.met_at_22))
 data.pfhxs.14.met_at_22$Age = rep("22", nrow(data.pfhxs.14.met_at_22))
 
 #### met at 28
 
-data.pfhxs.14.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfhxs\\pfhxs_14\\minerva_data_pfhxs_14_metabolites_28\\pfhxs_14_met_28_beta_fisher_C18.txt", header=TRUE)
+data.pfhxs.14.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/c18/pfhxs/pfhxs_14/minerva_data_pfhxs_14_metabolites_28/pfhxs_14_met_28_beta_fisher_C18.txt", header=TRUE)
 data.pfhxs.14.met_at_28$PFAS <-  rep("pfhxs", nrow(data.pfhxs.14.met_at_28))
 data.pfhxs.14.met_at_28$Age = rep("28", nrow(data.pfhxs.14.met_at_28))
 
@@ -135,5 +135,5 @@ pfhxs_met <- rbind(data.pfhxs.14.met_at_22, data.pfhxs.14.met_at_28)
 
 pfas_met_tab_C18 <- rbind(pfhxs_met,pfda_met,pfna_met,pfos_met,pfoa_met)
 pfas_met_tab_C18$Mode <- rep("C18", nrow(pfas_met_tab_C18))
-write.table(pfas_met_tab_C18, "C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\C18\\pfas_14_metabolites_all_C18.txt", row.names = F)
+write.table(pfas_met_tab_C18, "/sc/arion/projects/Faroese/pfas_met/c18/pfas_14_metabolites_all_C18.txt", row.names = F)
 
--- HILIC/pfas_0_metabolites_all_HILIC.R
+++ HILIC/pfas_0_metabolites_all_HILIC.R
@@ -61,25 +61,25 @@ library(ggrepel)
 ### pfoa age 0 
 #### met at 7
 
-data.pfoa.0.met_at_7 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfoa\\pfoa_0\\minerva_data_pfoa_0_metabolites_7\\pfoa_0_met_7_beta_fisher_hilic.txt", header=TRUE)
+data.pfoa.0.met_at_7 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfoa/pfoa_0/minerva_data_pfoa_0_metabolites_7/pfoa_0_met_7_beta_fisher_hilic.txt", header=TRUE)
 data.pfoa.0.met_at_7$PFAS <-  rep("pfoa", nrow(data.pfoa.0.met_at_7))
 data.pfoa.0.met_at_7$Age = rep("7", nrow(data.pfoa.0.met_at_7))
 
 #### met at 14
 
-data.pfoa.0.met_at_14 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfoa\\pfoa_0\\minerva_data_pfoa_0_metabolites_14\\pfoa_0_met_14_beta_fisher_hilic.txt", header=TRUE)
+data.pfoa.0.met_at_14 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfoa/pfoa_0/minerva_data_pfoa_0_metabolites_14/pfoa_0_met_14_beta_fisher_hilic.txt", header=TRUE)
 data.pfoa.0.met_at_14$PFAS <-  rep("pfoa", nrow(data.pfoa.0.met_at_14))
 data.pfoa.0.met_at_14$Age = rep("14", nrow(data.pfoa.0.met_at_14))
 
 #### met at 22
 
-data.pfoa.0.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfoa\\pfoa_0\\minerva_data_pfoa_0_metabolites_22\\pfoa_0_met_22_beta_fisher_hilic.txt", header=TRUE)
+data.pfoa.0.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfoa/pfoa_0/minerva_data_pfoa_0_metabolites_22/pfoa_0_met_22_beta_fisher_hilic.txt", header=TRUE)
 data.pfoa.0.met_at_22$PFAS <-  rep("pfoa", nrow(data.pfoa.0.met_at_22))
 data.pfoa.0.met_at_22$Age = rep("22", nrow(data.pfoa.0.met_at_22))
 
 #### met at 28
 
-data.pfoa.0.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfoa\\pfoa_0\\minerva_data_pfoa_0_metabolites_28\\pfoa_0_met_28_beta_fisher_hilic.txt", header=TRUE)
+data.pfoa.0.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfoa/pfoa_0/minerva_data_pfoa_0_metabolites_28/pfoa_0_met_28_beta_fisher_hilic.txt", header=TRUE)
 data.pfoa.0.met_at_28$PFAS <-  rep("pfoa", nrow(data.pfoa.0.met_at_28))
 data.pfoa.0.met_at_28$Age = rep("28", nrow(data.pfoa.0.met_at_28))
 
@@ -89,25 +89,25 @@ pfoa_met <- rbind(data.pfoa.0.met_at_7,data.pfoa.0.met_at_14, data.pfoa.0.met_at
 ### pfos age 0 
 #### met at 7
 
-data.pfos.0.met_at_7 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfos\\pfos_0\\minerva_data_pfos_0_metabolites_7\\pfos_0_met_7_beta_fisher_hilic.txt", header=TRUE)
+data.pfos.0.met_at_7 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfos/pfos_0/minerva_data_pfos_0_metabolites_7/pfos_0_met_7_beta_fisher_hilic.txt", header=TRUE)
 data.pfos.0.met_at_7$PFAS <-  rep("pfos", nrow(data.pfos.0.met_at_7))
 data.pfos.0.met_at_7$Age = rep("7", nrow(data.pfos.0.met_at_7))
 
 #### met at 14
 
-data.pfos.0.met_at_14 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfos\\pfos_0\\minerva_data_pfos_0_metabolites_14\\pfos_0_met_14_beta_fisher_hilic.txt", header=TRUE)
+data.pfos.0.met_at_14 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfos/pfos_0/minerva_data_pfos_0_metabolites_14/pfos_0_met_14_beta_fisher_hilic.txt", header=TRUE)
 data.pfos.0.met_at_14$PFAS <-  rep("pfos", nrow(data.pfos.0.met_at_14))
 data.pfos.0.met_at_14$Age = rep("14", nrow(data.pfos.0.met_at_14))
 
 #### met at 22
 
-data.pfos.0.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfos\\pfos_0\\minerva_data_pfos_0_metabolites_22\\pfos_0_met_22_beta_fisher_hilic.txt", header=TRUE)
+data.pfos.0.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfos/pfos_0/minerva_data_pfos_0_metabolites_22/pfos_0_met_22_beta_fisher_hilic.txt", header=TRUE)
 data.pfos.0.met_at_22$PFAS <-  rep("pfos", nrow(data.pfos.0.met_at_22))
 data.pfos.0.met_at_22$Age = rep("22", nrow(data.pfos.0.met_at_22))
 
 #### met at 28
 
-data.pfos.0.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfos\\pfos_0\\minerva_data_pfos_0_metabolites_28\\pfos_0_met_28_beta_fisher_hilic.txt", header=TRUE)
+data.pfos.0.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfos/pfos_0/minerva_data_pfos_0_metabolites_28/pfos_0_met_28_beta_fisher_hilic.txt", header=TRUE)
 data.pfos.0.met_at_28$PFAS <-  rep("pfos", nrow(data.pfos.0.met_at_28))
 data.pfos.0.met_at_28$Age = rep("28", nrow(data.pfos.0.met_at_28))
 
@@ -116,25 +116,25 @@ pfos_met <- rbind(data.pfos.0.met_at_7,data.pfos.0.met_at_14, data.pfos.0.met_at
 ### pfna age 0 
 #### met at 7
 
-data.pfna.0.met_at_7 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfna\\pfna_0\\minerva_data_pfna_0_metabolites_7\\pfna_0_met_7_beta_fisher_hilic.txt", header=TRUE)
+data.pfna.0.met_at_7 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfna/pfna_0/minerva_data_pfna_0_metabolites_7/pfna_0_met_7_beta_fisher_hilic.txt", header=TRUE)
 data.pfna.0.met_at_7$PFAS <-  rep("pfna", nrow(data.pfna.0.met_at_7))
 data.pfna.0.met_at_7$Age = rep("7", nrow(data.pfna.0.met_at_7))
 
 #### met at 14
 
-data.pfna.0.met_at_14 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfna\\pfna_0\\minerva_data_pfna_0_metabolites_14\\pfna_0_met_14_beta_fisher_hilic.txt", header=TRUE)
+data.pfna.0.met_at_14 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfna/pfna_0/minerva_data_pfna_0_metabolites_14/pfna_0_met_14_beta_fisher_hilic.txt", header=TRUE)
 data.pfna.0.met_at_14$PFAS <-  rep("pfna", nrow(data.pfna.0.met_at_14))
 data.pfna.0.met_at_14$Age = rep("14", nrow(data.pfna.0.met_at_14))
 
 #### met at 22
 
-data.pfna.0.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfna\\pfna_0\\minerva_data_pfna_0_metabolites_22\\pfna_0_met_22_beta_fisher_hilic.txt", header=TRUE)
+data.pfna.0.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfna/pfna_0/minerva_data_pfna_0_metabolites_22/pfna_0_met_22_beta_fisher_hilic.txt", header=TRUE)
 data.pfna.0.met_at_22$PFAS <-  rep("pfna", nrow(data.pfna.0.met_at_22))
 data.pfna.0.met_at_22$Age = rep("22", nrow(data.pfna.0.met_at_22))
 
 #### met at 28
 
-data.pfna.0.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfna\\pfna_0\\minerva_data_pfna_0_metabolites_28\\pfna_0_met_28_beta_fisher_hilic.txt", header=TRUE)
+data.pfna.0.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfna/pfna_0/minerva_data_pfna_0_metabolites_28/pfna_0_met_28_beta_fisher_hilic.txt", header=TRUE)
 data.pfna.0.met_at_28$PFAS <-  rep("pfna", nrow(data.pfna.0.met_at_28))
 data.pfna.0.met_at_28$Age = rep("28", nrow(data.pfna.0.met_at_28))
 
@@ -144,25 +144,25 @@ pfna_met <- rbind(data.pfna.0.met_at_7,data.pfna.0.met_at_14, data.pfna.0.met_at
 ### pfda age 0 
 #### met at 7
 
-data.pfda.0.met_at_7 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfda\\pfda_0\\minerva_data_pfda_0_metabolites_7\\pfda_0_met_7_beta_fisher_hilic.txt", header=TRUE)
+data.pfda.0.met_at_7 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfda/pfda_0/minerva_data_pfda_0_metabolites_7/pfda_0_met_7_beta_fisher_hilic.txt", header=TRUE)
 data.pfda.0.met_at_7$PFAS <-  rep("pfda", nrow(data.pfda.0.met_at_7))
 data.pfda.0.met_at_7$Age = rep("7", nrow(data.pfda.0.met_at_7))
 
 #### met at 14
 
-data.pfda.0.met_at_14 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfda\\pfda_0\\minerva_data_pfda_0_metabolites_14\\pfda_0_met_14_beta_fisher_hilic.txt", header=TRUE)
+data.pfda.0.met_at_14 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfda/pfda_0/minerva_data_pfda_0_metabolites_14/pfda_0_met_14_beta_fisher_hilic.txt", header=TRUE)
 data.pfda.0.met_at_14$PFAS <-  rep("pfda", nrow(data.pfda.0.met_at_14))
 data.pfda.0.met_at_14$Age = rep("14", nrow(data.pfda.0.met_at_14))
 
 #### met at 22
 
-data.pfda.0.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfda\\pfda_0\\minerva_data_pfda_0_metabolites_22\\pfda_0_met_22_beta_fisher_hilic.txt", header=TRUE)
+data.pfda.0.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfda/pfda_0/minerva_data_pfda_0_metabolites_22/pfda_0_met_22_beta_fisher_hilic.txt", header=TRUE)
 data.pfda.0.met_at_22$PFAS <-  rep("pfda", nrow(data.pfda.0.met_at_22))
 data.pfda.0.met_at_22$Age = rep("22", nrow(data.pfda.0.met_at_22))
 
 #### met at 28
 
-data.pfda.0.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfda\\pfda_0\\minerva_data_pfda_0_metabolites_28\\pfda_0_met_28_beta_fisher_hilic.txt", header=TRUE)
+data.pfda.0.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfda/pfda_0/minerva_data_pfda_0_metabolites_28/pfda_0_met_28_beta_fisher_hilic.txt", header=TRUE)
 data.pfda.0.met_at_28$PFAS <-  rep("pfda", nrow(data.pfda.0.met_at_28))
 data.pfda.0.met_at_28$Age = rep("28", nrow(data.pfda.0.met_at_28))
 
@@ -172,25 +172,25 @@ pfda_met <- rbind(data.pfda.0.met_at_7,data.pfda.0.met_at_14, data.pfda.0.met_at
 ### pfhxs age 0 
 #### met at 7
 
-data.pfhxs.0.met_at_7 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfhxs\\pfhxs_0\\minerva_data_pfhxs_0_metabolites_7\\pfhxs_0_met_7_beta_fisher_hilic.txt", header=TRUE)
+data.pfhxs.0.met_at_7 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfhxs/pfhxs_0/minerva_data_pfhxs_0_metabolites_7/pfhxs_0_met_7_beta_fisher_hilic.txt", header=TRUE)
 data.pfhxs.0.met_at_7$PFAS <-  rep("pfhxs", nrow(data.pfhxs.0.met_at_7))
 data.pfhxs.0.met_at_7$Age = rep("7", nrow(data.pfhxs.0.met_at_7))
 
 #### met at 14
 
-data.pfhxs.0.met_at_14 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfhxs\\pfhxs_0\\minerva_data_pfhxs_0_metabolites_14\\pfhxs_0_met_14_beta_fisher_hilic.txt", header=TRUE)
+data.pfhxs.0.met_at_14 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfhxs/pfhxs_0/minerva_data_pfhxs_0_metabolites_14/pfhxs_0_met_14_beta_fisher_hilic.txt", header=TRUE)
 data.pfhxs.0.met_at_14$PFAS <-  rep("pfhxs", nrow(data.pfhxs.0.met_at_14))
 data.pfhxs.0.met_at_14$Age = rep("14", nrow(data.pfhxs.0.met_at_14))
 
 #### met at 22
 
-data.pfhxs.0.met_at_22 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfhxs\\pfhxs_0\\minerva_data_pfhxs_0_metabolites_22\\pfhxs_0_met_22_beta_fisher_hilic.txt", header=TRUE)
+data.pfhxs.0.met_at_22 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfhxs/pfhxs_0/minerva_data_pfhxs_0_metabolites_22/pfhxs_0_met_22_beta_fisher_hilic.txt", header=TRUE)
 data.pfhxs.0.met_at_22$PFAS <-  rep("pfhxs", nrow(data.pfhxs.0.met_at_22))
 data.pfhxs.0.met_at_22$Age = rep("22", nrow(data.pfhxs.0.met_at_22))
 
 #### met at 28
 
-data.pfhxs.0.met_at_28 <- read.table("C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfhxs\\pfhxs_0\\minerva_data_pfhxs_0_metabolites_28\\pfhxs_0_met_28_beta_fisher_hilic.txt", header=TRUE)
+data.pfhxs.0.met_at_28 <- read.table("/sc/arion/projects/Faroese/pfas_met/hilic/pfhxs/pfhxs_0/minerva_data_pfhxs_0_metabolites_28/pfhxs_0_met_28_beta_fisher_hilic.txt", header=TRUE)
 data.pfhxs.0.met_at_28$PFAS <-  rep("pfhxs", nrow(data.pfhxs.0.met_at_28))
 data.pfhxs.0.met_at_28$Age = rep("28", nrow(data.pfhxs.0.met_at_28))
 
@@ -199,5 +199,5 @@ pfhxs_met <- rbind(data.pfhxs.0.met_at_7,data.pfhxs.0.met_at_14, data.pfhxs.0.me
 
 pfas_met_tab_hilic <- rbind(pfhxs_met,pfda_met,pfna_met,pfos_met,pfoa_met)
 pfas_met_tab_hilic$Mode <- rep("HILIC", nrow(pfas_met_tab_hilic))
-write.table(pfas_met_tab_hilic, "C:\\Users\\yaom03\\OneDrive - The Mount Sinai Hospital\\New_faroese\\HILIC\\pfas_0_metabolites_all_HILIC.txt", row.names = F)
+write.table(pfas_met_tab_hilic, "/sc/arion/projects/Faroese/pfas_met/hilic/pfas_0_metabolites_all_HILIC.txt", row.names = F)
 
